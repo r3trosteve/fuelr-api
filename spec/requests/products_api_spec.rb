8eba9aa3ed9899db317f643b9b0354b236560e53
Steve Schofield <stevebschofield@gmail.com>
2014-02-25 14:00:00 +0000

    specify API behaviour with one product

--- spec/requests/products_api_spec.rb
+++ spec/requests/products_api_spec.rb
@@ -2,10 +2,26 @@ require 'spec_helper'
 
 describe Fuelr::ProductApi do
   describe "GET /api/v1/products" do
-    it "returns a list of products" do
-      response.status.should be(200)
-      expect(JSON.parse(response.body)).to eq []
-    end
+
+  	context "without any product" do
+	    it "returns a list of products" do
+  			get "/api/v1/products"
+	      response.status.should be(200)
+	      expect(JSON.parse(response.body)).to eq []
+	    end
+  	end
+
+  	context "with one product" do
+  		before do
+  		  product = Product.create! name: "Stretching", description: "Is good for you"
+  		end
+  		it "returns a list of products" do
+  		  get "/api/v1/products"
+	      response.status.should be(200)
+	      result = JSON.parse(response.body)
+	      expect(result.first["name"]).to eq("Stretching")
+	    end
+  	end
   end
 end
 
